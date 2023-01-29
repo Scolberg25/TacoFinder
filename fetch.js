f3756631ef0f1a68dceaf71e046ad37214cedcd9
blastcraftmc <scolberg@proton.me>
2023-01-29 12:35:30 -0500

    Misc.

--- fetch.js
+++ fetch.js
@@ -15,24 +15,41 @@ fetch("https://api.apispreadsheets.com/data/fkB8dNLc0Znfg5FF/").then(res => {
             for (let i = 0; i < yourData.length; i++) {
                 let rowInfo = yourData[i]
 
+                //Creating Div
                 let rowInfoDiv = document.createElement("div")
                 rowInfoDiv.classList.add("name-row")
 
+                //ID (h4)
 				let rowId = document.createElement("h4")
                 let rowIdNode = document.createTextNode(rowInfo["id"])
                 rowId.appendChild(rowIdNode)
                 rowId.classList.add("id")
 
+                //Name (p)
                 let rowName = document.createElement("p")
                 let rowNameNode = document.createTextNode(rowInfo["name"])
                 rowName.appendChild(rowNameNode)
                 rowName.classList.add("name")
 
+                //Image (img)
+                let rowImg = document.createElement("img")
+                let rowImgNode = document.createTextNode(rowInfo["image"])
+                rowImg.setAttribute('src', rowImgNode);
+                rowImg.classList.add("image")
+
+                //Location Image (img)
+                let rowImgLoc = document.createElement("img")
+                let rowImgLocNode = document.createTextNode(rowInfo["locationlink"])
+                rowImgLoc.setAttribute('src', '\images\marker.svg');
+                rowImgLoc.classList.add("locationlink")
+
+                //Location (p)
                 let rowLocation = document.createElement("p")
                 let rowLocationNode = document.createTextNode(rowInfo["location"])
                 rowLocation.appendChild(rowLocationNode)
                 rowLocation.classList.add("location")
 
+                //Stars (p)
 				let rowStars = document.createElement("p")
                 let rowStarsNode = document.createTextNode(rowInfo["stars"])
                 rowStars.appendChild(rowStarsNode)
