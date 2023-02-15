1543e184dbd6969a46b2657a5f8164dbcea2dec8
blastcraftmc <scolberg@proton.me>
2023-02-15 17:17:10 -0500

    Misc.

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/home.html');
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/about.html');
+app.get('/info', (req, res) => {
+    res.sendFile(__dirname + '/info.html');
 });
 
 server.listen(PORT, () => {
