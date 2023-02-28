4483f438df28b4c42a05818f8634ad8f56a9d80e
blastcraftmc <scolberg@proton.me>
2023-02-28 15:03:12 -0500

    Clear button works

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-/*document.getElementById('clearButton').addEventListener('click', function() {
-    let allNamesElm = document.getElementsByClassName("name-row");
-    allNamesElm.classList.add("showing");
-    window.alert("Cleared!");
-    console.log("cleared");
-});*/
+function clearButton() {
+    document.getElementById("location-select").selectedIndex = 0;
+    document.getElementById("vibe-select").selectedIndex = 0;
+    document.getElementById("stars-select").selectedIndex = 0;
+    let allNameRows = document.querySelectorAll(".name-row");
+    //console.log(allNameRows)
+
+    for (let i = 0; i < allNameRows.length; i++) {
+        let row = allNameRows[i]
+        row.classList.add("showing");
+    }
+
+    let chip = document.getElementById('clearChip');
+    chip.style.visibility = "visible";
+    chip.classList.add("clearChipSelected");
+    setTimeout(() => { chip.style.visibility = "hidden"; chip.classList.remove("clearChipSelected"); }, 5200);
+}
 
 //Main
 function prevPage() {
